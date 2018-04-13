a66f592fbed6c71ed978008c8319b244aae1ac05
Daniel Doza <doza.daniel@gmail.com>
2018-04-13 02:54:20 +0200

    switch go workspace name

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ set -x EDITOR /usr/bin/vim
 set -x GIT_TERMINAL_PROMPT 1
 
 set -x GOROOT /usr/local/go
-set -x GOPATH /home/daniel/GoWorkspace
+set -x GOPATH /home/daniel/GoLang
 
 set -x PATH $GOPATH $PATH
 set -x PATH $GOROOT/bin $PATH
