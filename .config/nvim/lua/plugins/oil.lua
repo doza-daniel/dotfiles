954986de82d270ab9586842781c8813bf81cd26f
doza-daniel <doza.daniel@gmail.com>
2025-01-28 17:23:55 +0100

    Add more columns to oiil view

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,10 @@
 return {
   'stevearc/oil.nvim',
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = function()
     vim.keymap.set("n", "-", "<CMD>Oil<CR>")
+    return {
+      columns = { "icon", "permissions", "size", "mtime" }
+    }
   end
 }
