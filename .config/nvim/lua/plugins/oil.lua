3bb8d6aecaa5ee4126123b9d7dbac93b8c3769e4
Daniel Doza <doza.daniel@gmail.com>
2025-05-19 00:31:41 +0200

    cp oil cfg from mac

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,9 +1,10 @@
 return {
   'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = function()
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>")
+    return {
+      columns = { "icon", "permissions", "size", "mtime" }
+    }
+  end
 }
