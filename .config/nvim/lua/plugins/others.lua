302e356dd0327050ae84378044812d6b0e75f8c5
Daniel Doza <doza.daniel@gmail.com>
2025-05-22 19:10:49 +0200

    Add gitgutter

--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -1,5 +1,16 @@
 return {
   "tpope/vim-fugitive",
+  {
+    "airblade/vim-gitgutter",
+    config = function()
+      vim.api.nvim_create_autocmd("BufWritePost", {
+        callback = function()
+          vim.cmd('GitGutter')
+        end
+      })
+      vim.o.updatetime = 200
+    end
+  },
   { "tpope/vim-rhubarb", dependencies = { "tpope/vim-fugitive" } },
   { "ellisonleao/gruvbox.nvim", priority = 1000 }
 }
