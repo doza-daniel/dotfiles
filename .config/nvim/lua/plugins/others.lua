84de659774e109b8c3ee68ebc559218f9ab3f904
Daniel Doza <doza.daniel@gmail.com>
2025-06-04 00:19:53 +0200

    Add catpuccin neovim theme

--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -17,5 +17,9 @@ return {
     end
   },
   { "tpope/vim-rhubarb", dependencies = { "tpope/vim-fugitive" } },
-  { "ellisonleao/gruvbox.nvim", priority = 1000 }
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000
+  },
 }
