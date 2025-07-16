94b826abcf711d895b9840b3a4fc66063b98b447
doza-daniel <doza.daniel@gmail.com>
2025-07-16 15:26:09 +0200

    nvim - add catppuccin and rhubarb

--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -1,4 +1,10 @@
 return {
   "tpope/vim-fugitive",
-  { "ellisonleao/gruvbox.nvim", priority = 1000 }
+  { "tpope/vim-rhubarb", dependencies = { "tpope/vim-fugitive" } },
+  { "ellisonleao/gruvbox.nvim", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000
+  },
 }
