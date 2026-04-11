2cfe0b42de6478b9bb79f24dcb256c90e06ee70b
Daniel Doza <doza.daniel@gmail.com>
2026-04-12 01:14:48 +0200

    Add js treesitter parser

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "javascript", "go", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+      ensure_installed = { "python", "javascript", "go", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
