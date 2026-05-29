170bfe6ec69d82a3bc458f5c1b3e7ac2177a5a96
doza-daniel <doza.daniel@gmail.com>
2026-05-29 17:33:44 +0200

    Change nvim colors based on ghostty

--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -1,13 +1,45 @@
-vim.api.nvim_create_autocmd("ColorScheme", {
-  group = vim.api.nvim_create_augroup('on-colorscheme-change', { clear = true }),
+local auGroup = vim.api.nvim_create_augroup('on-colorscheme-change', { clear = true })
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  group = auGroup,
   callback = function()
     -- remove background from colorscheme
-    vim.cmd.highlight("Normal ctermbg=none guibg=none")
-    vim.cmd.highlight("SignColumn ctermbg=none guibg=none")
-    vim.cmd.highlight("NonText ctermbg=none guibg=none")
-    vim.cmd.highlight("LineNr ctermbg=none guibg=none")
-    vim.cmd.highlight("VertSplit ctermbg=none guibg=none")
+    -- vim.cmd.highlight("Normal ctermbg=none guibg=none")
+    -- vim.cmd.highlight("SignColumn ctermbg=none guibg=none")
+    -- vim.cmd.highlight("NonText ctermbg=none guibg=none")
+    -- vim.cmd.highlight("LineNr ctermbg=none guibg=none")
+    -- vim.cmd.highlight("VertSplit ctermbg=none guibg=none")
   end
 })
 
-vim.cmd.colorscheme("catppuccin-mocha")
+local ghosttyToNvim = {
+  ["gruvbox light"] = "gruvbox",
+  ["gruvbox dark"] = "gruvbox",
+  ["catppuccin frappe"] = "catppuccin-frappe",
+  ["catppuccin latte"] = "catppuccin-latte",
+  ["catppuccin macchiato"] = "catppuccin-macchiato",
+  ["catppuccin mocha"] = "catppuccin-mocha",
+}
+
+local setColorscheme = function()
+  local theme = 'default'
+
+  local ghosttyConfig = vim.system({ "ghostty", "+show-config" }, { text = true }):wait().stdout
+
+  if ghosttyConfig then
+    for s in ghosttyConfig:gmatch("theme = [^\n]+") do
+      local parsed, _ = s:gsub("theme = ", "")
+      theme = ghosttyToNvim[parsed:lower()] or theme
+    end
+  end
+
+  vim.cmd.colorscheme(theme)
+end
+
+setColorscheme()
+
+vim.api.nvim_create_autocmd('OptionSet', {
+  group = auGroup,
+  pattern = 'background',
+  callback = setColorscheme,
+})
